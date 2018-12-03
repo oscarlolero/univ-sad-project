001710cb274501a328763788dd468134be409a26
Oscar Montes <o.montescamberos@ugto.mx>
2018-12-02 22:54:27 -0600

    Final commit

--- src/config/passport.js
+++ src/config/passport.js
@@ -14,48 +14,7 @@ module.exports = function(passport) {
         //console.log('Deserializing user: ', user);
         done(null, user);
      });
-    // para registrarse
-    // passport.use('local-signup', new LocalStrategy({
-    //     usernameField: 'username',
-    //     passwordField: 'password',
-    //     passReqToCallback: true
-    // }, function(req, username, password, done) {
-    //     User.findOne({'local.username': username}, function (err, user) {
-    //         if(err) { 
-    //             return done(err); 
-    //         }
-    //         if(user) {
-    //             return done(null, false, req.flash('signupMessage', 'The email is already taken.')); //(sin error, sin usuario, mensaje)
-    //         } else {
-    //             var newUser = new User();
-    //             newUser.local.username = username;
-    //             newUser.local.password = newUser.generateHash(password);
-    //             newUser.save(function (err) {
-    //                 if(err) {throw err;} //aqui se puede usar promesas o asyncawait
-    //                 return done(null, newUser);
-    //             });
-    //         }
-    //     })
-    // }));
 
-    // // para login
-    // passport.use('local-login', new LocalStrategy({
-    //     usernameField: 'username',
-    //     passwordField: 'password',
-    //     passReqToCallback: true
-    // }, function(req, username, password, done) {
-    //     User.findOne({'local.username': username}, function (err, user) {
-    //         if(err) { return done(err); }
-    //         if(!user) {
-    //             return done(null, false, req.flash('loginMessage', 'El usuario no existe.')); //(sin error, sin usuario, mensaje)
-    //         }
-    //         if(!user.validPassword(password)) {//si la pass es falsa, nop coincide
-    //             return done(null, false, req.flash('loginMessage', 'Wrong pass.'));
-    //         }
-    //         return done(null, user);
-    //     })
-    // }));
-    // para login MS SQL SERVER
     passport.use('local-login', new LocalStrategy({
         usernameField: 'username',
         passwordField: 'password',
